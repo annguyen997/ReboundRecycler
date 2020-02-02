ea9c5cac5cd3bda265c837010faa92552ced0502
keaysma <michaelandrewkeays@gmail.com>
2020-02-01 21:42:25 -0500

    Fixes to CORS policy, and main page changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,37 +1,19 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import map from './map';
-import {Loader, LoaderOptions} from 'google-maps';
+import Listings from './bounty-listings';
+import Map from './map';
 import './App.css';
 
 class App extends Component{
-
-  
-
   constructor(props){
     super(props)
-    this.map = this.map.bind(this)
-
-    const options: LoaderOptions = {/* todo */};
-    this.loader = new Loader(null, options);
-
-  }
-
-  map = () => {
-    this.loader.load().then(function (google) {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8,
-      });
-    });
   }
 
   render(){
     return (
       <div id="overlay">
-        <div id="map"> 
-          {this.map()}
-        </div>
+	  	<Map/>
+		<Listings/>
         <div id ="overlay-border">
         </div>
         <p className= "opaque-text"> Rebound </p>
